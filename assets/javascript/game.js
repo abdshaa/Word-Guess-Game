16df8cdcedff73a1f59d444df18d4a5ed1d5565b
abdul shaikh <raaqibshaikh@yahoo.com>
2019-08-12 20:36:35 -0500

    game

---
+++ assets/javascript/game.js
@@ -0,0 +1,64 @@
+//array for state list
+const stateList = [
+  "Alaska",
+  "Alabama",
+  "Arkansas",
+  "Arizona",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Iowa",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Massachusetts",
+  "Maryland",
+  "Maine",
+  "Michigan",
+  "Minnesota",
+  "Missouri",
+  "Mississippi",
+  "Montana",
+  "North Carolina",
+  "North Dakota",
+  "Nebraska",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "Nevada",
+  "New York",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Virginia",
+  "Vermont",
+  "Washington",
+  "Wisconsin",
+  "West Virginia",
+  "Wyoming"
+];
+//variables
+let win = 0;
+let lose = 0;
+let wrongGuesses = 12;
+let computerGuess = getstateList();
+let letterGuessed = "";
+//Play //
+document.onkeyup = function(event) {
+  userGuess = event.key.toLowerCase;
+  letterGuessed(userGuess);
+};
